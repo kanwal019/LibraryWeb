5d0ed846dba27cf3b258874257d485a9b0db37e6
kanwal019 <kanwal019@gmail.com>
2021-01-07 19:09:15 +0530

    Initial cleanup and created a main component

---
+++ src/app/library-main/library-main.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LibraryMainComponent } from './library-main.component';
+
+describe('LibraryMainComponent', () => {
+  let component: LibraryMainComponent;
+  let fixture: ComponentFixture<LibraryMainComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LibraryMainComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LibraryMainComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
